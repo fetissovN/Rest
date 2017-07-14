a0a87ce337754e0fa71bb39f7e23cf365fdf903d
Николай <fetissov.n@gmail.com>
2017-07-14 13:18:01 +0300

    config app

--- src/main/java/com/rest/ApplicationInitializer.java
+++ src/main/java/com/rest/ApplicationInitializer.java
@@ -1,4 +1,28 @@
 package com.rest;
 
-public class ApplicationInitializer {
+import com.rest.config.WebConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+
+    private static final String DISPATCHER = "dispatcher";
+
+
+    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(WebConfig.class);
+
+        servletContext.addListener(new ContextLoaderListener(ctx));
+
+        ServletRegistration.Dynamic servet = servletContext.addServlet(DISPATCHER, new DispatcherServlet(ctx));
+        servet.addMapping("/");
+        servet.setLoadOnStartup(1);
+
+    }
 }
--- src/main/java/com/rest/controller/RemindController.java
+++
@@ -1,6 +0,0 @@
-package com.rest.controller;
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class RemindController {
-}
