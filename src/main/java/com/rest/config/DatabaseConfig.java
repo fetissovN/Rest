8fd87902784905c74210f5006c36c53831eceb70
Николай <fetissov.n@gmail.com>
2017-07-14 19:02:17 +0300

    4 db

--- src/main/java/com/rest/config/DatabaseConfig.java
+++ src/main/java/com/rest/config/DatabaseConfig.java
@@ -1,21 +1,36 @@
 package com.rest.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories("com.rest.repository")
 @EnableTransactionManagement
+@PropertySource("classpath:db.properties")
 @ComponentScan("com.rest")
 public class DatabaseConfig {
 
     @Resource
     private Environment env;
 
+    @Bean
+    public DataSource dataSource(){
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(env.getRequiredProperty("db.url"));
+        dataSource.setDriverClassName(env.getRequiredProperty("db.driver"));
+        dataSource.setUsername(env.getRequiredProperty("db.username"));
+        dataSource.setPassword(env.getRequiredProperty("db.password"));
+
+        return dataSource;
+    }
 
 }
