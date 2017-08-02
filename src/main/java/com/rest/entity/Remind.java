b8f47b47e40de075c65f8a71c94af1045fd229a3
fetissovN <fetissov.n@gmail.com>
2017-08-02 17:30:54 +0300

    db

--- src/main/java/com/rest/entity/Remind.java
+++ src/main/java/com/rest/entity/Remind.java
@@ -6,8 +6,45 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "voter")
+@Table(name = "remind")
 public class Remind {
 
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(strategy = "increment",name = "increment")
+    private Long id;
 
+    @Column(name = "title", nullable = false, length = 50)
+    private String title;
+
+    @Column(name = "remind_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date remindDate;
+
+    public Remind() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getRemindDate() {
+        return remindDate;
+    }
+
+    public void setRemindDate(Date remindDate) {
+        this.remindDate = remindDate;
+    }
 }
